4791fdfd3734546f28e8c214912efec9393b8d6f
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-02-14 11:45:02 +0300

    5th

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author =models.CharField(max_length=100)
-    publication_year = models.IntegerField
+    title = models.CharField(max_length=200, null=False)
+    author =models.CharField(max_length=100 ,null=False)
+    publication_year = models.IntegerField(null=False)
 
-# book = Book(title='1984', author='George Orwell', publication_year=1949)
-# book.save()
