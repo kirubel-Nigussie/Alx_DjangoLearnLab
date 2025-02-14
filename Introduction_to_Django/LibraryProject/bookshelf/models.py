2adf178578ebc7168410e63d4bec14a08ef75b4f
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-02-14 09:22:09 +0300

    6th

---
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author =models.CharField(max_length=100)
+    publication_year = models.IntegerField
+
+# book = Book(title='1984', author='George Orwell', publication_year=1949)
+# book.save()
