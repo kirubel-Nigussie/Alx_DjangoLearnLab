64cfa897c9ef5b39b7f0201c17fc15f8a2da5122
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-02-24 19:15:21 +0300

    5th

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView , register , LoginView, LogoutView
 
 
 urlpatterns = [
     path("books/", view = list_books, name= "book_list"),
-    path("details/", view = LibraryDetailView.as_view(), name="LibraryDetailView")
+    path("details/", view = LibraryDetailView.as_view(), name="LibraryDetailView"),
+
+    path('register/', view = register.as_view, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    # path('login/', user_login, name='login'),
+    # path('logout/', user_logout, name='logout'),
 
 ] 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,15 @@ from .models import Library
 from django.views.generic.detail import DetailView
 
 
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+
+
 
 def list_books(request):
     books = Book.objects.all() 
@@ -15,4 +24,26 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "Libraris"
+
+
+class register(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html' 
+
+# class LoginView():
+#     pass   
+
+# class LogoutView():
+#     pass
+
+# urlpatterns = [
+#     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+# ]  
+
+# urlpatterns = [
+#     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+# ]
+
+
 
