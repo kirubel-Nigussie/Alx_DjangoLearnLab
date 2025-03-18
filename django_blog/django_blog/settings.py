0754c60bb5b71149d74bdae60f6917ca82792647
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-03-18 12:26:46 +0300

    4th commit

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,10 +75,19 @@ WSGI_APPLICATION = "django_blog.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+
+      'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',  
+        'PASSWORD': '0984734217',
+        'HOST': 'localhost', 
+        'PORT': '5432', 
+      }
 }
 
 
