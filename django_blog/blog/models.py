0d77eef3e1a51c3bfaff6262b4f3cf71f470cb7e
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-03-18 12:11:28 +0300

    3rd commit

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post (models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=100)
     published_date = models.DateField(auto_now_add=True)
-    author: models.ForeignKey
+    author= models.ForeignKey(User, on_delete= models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 
