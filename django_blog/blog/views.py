f18a90af7a5dfcb74f14e269746a3eef25980676
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-03-19 08:23:46 +0300

    2nd

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.hashers import check_password
+from .forms import ProfileUpdateForm
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
@@ -30,4 +34,34 @@ def user_logout(request):
 
 
 
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+    return render(request, 'blog/profile.html', {'form': form})
+
+
+
+
+def verify_password(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return render(request, 'blog/verify_password.html', {'error': 'User not found'})
+
+        if check_password(password, user.password):  # Verify the password
+            return render(request, 'blog/verify_password.html', {'message': 'Password is correct!'})
+        else:
+            return render(request, 'blog/verify_password.html', {'error': 'Incorrect password!'})
+
+    return render(request, 'blog/verify_password.html')
 
