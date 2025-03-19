0f26fa6b1cca58c90e727f365797ad7479bf6d5d
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-03-19 09:10:09 +0300

    23th

--- api_project1/api_project/api_project/urls.py
+++ api_project1/api_project/api_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for advanced_api_project project.
+URL configuration for api_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
