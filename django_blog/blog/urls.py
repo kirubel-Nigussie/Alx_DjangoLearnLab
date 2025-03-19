054ff373f56c84331b5ce9a147f26fbdd8c6b056
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-03-19 14:03:30 +0300

    6th

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('post/<int:pk>/comment/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path("post/<int:pk>/comments/new/")
 
 ]
 
