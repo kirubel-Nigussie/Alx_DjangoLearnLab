d832c7c0cb4f1638e7ad406fd3f6f87f0c7e6479
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-04-06 17:55:30 +0300

    7th

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -32,3 +32,37 @@ class ProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+    
+
+
+
+
+
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    try:
+        user_to_follow = User.objects.get(id=user_id)
+        request.user.following.add(user_to_follow)
+        return Response({'detail': f'You are now following {user_to_follow.username}.'})
+    except User.DoesNotExist:
+        return Response({'error': 'User not found'}, status=404)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    try:
+        user_to_unfollow = User.objects.get(id=user_id)
+        request.user.following.remove(user_to_unfollow)
+        return Response({'detail': f'You have unfollowed {user_to_unfollow.username}.'})
+    except User.DoesNotExist:
+        return Response({'error': 'User not found'}, status=404)
+
